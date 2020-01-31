6d1d640940bbd77aa8e1eb3713c60e6ea818e1a1
Kang, Zixuan <zkang35@gatech.edu>
2020-01-31 10:20:14 -0500

    Ready to deploy

---
+++ client/build/precache-manifest.892a9af98439029cdf786d61a97ae3c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d489690e2a0222dd3bbf81afab1308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8343a95c2fe9f38a12ab",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "f65179a14a6452fd1696",
+    "url": "/static/css/main.df3736d7.chunk.css"
+  },
+  {
+    "revision": "8343a95c2fe9f38a12ab",
+    "url": "/static/js/2.335d6844.chunk.js"
+  },
+  {
+    "revision": "680de19adb8c8cfedc28fbc639d12826",
+    "url": "/static/js/2.335d6844.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f65179a14a6452fd1696",
+    "url": "/static/js/main.03dad6e5.chunk.js"
+  },
+  {
+    "revision": "f0cbfb9d9df2c2377d11",
+    "url": "/static/js/runtime-main.c14c325a.js"
+  }
+]);
